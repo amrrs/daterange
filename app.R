9a369a1c30f1022b4669f1a59bed75ff64f83572
joebrew <joebrew@gmail.com>
2018-02-11 00:00:32 +0100

    figured out date rnage stuff!

--- app.R
+++ app.R
@@ -32,21 +32,7 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      tags$div(
-        HTML(
-          " 
-          <input type=\"text\" name=\"daterange\" value=\"01/05/2015 - 01/31/2015\" />
-          
-          <script type=\"text/javascript\">
-          $(function() {
-          $('input[name=\"daterange\"]').daterangepicker();
-// the below two lines were written by joe, trying to get the input list
-// to get updated, but it's not working
-          var vv = ('input[name=\"daterange\"]').daterangepicker();
-          Shiny.onInputChange('daterange', vv);
-          });
-          </script>")
-        )
+      uiOutput('date_ui')
       ),
     
     # Show a plot of the generated distribution
@@ -59,11 +45,47 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$date_range_values <- renderText({
-    # This is working on the start of the app, but doesn't update
-    as.character(input$daterange)
+  date_range <- reactiveVal(c(Sys.Date() - 7,
+                              Sys.Date() + 7))
+  
+  observeEvent(input$daterange, {
+    message('input date range is ', input$daterange)
+    idr <- input$daterange
+    idr <- unlist(strsplit(idr, ' - '))
+    idr <- as.Date(idr, format = '%m/%d/%Y')
+    date_range(idr)
   })
-
+  
+  output$date_range_values <- renderText({
+    as.character(date_range())
+  })
+  
+  output$date_ui <- renderUI({
+    dr <- date_range()
+    dr <- as.Date(dr)
+    print(dr)
+    dr <- format(dr, '%m/%d/%Y')
+    tags$div(
+      HTML(
+        paste0(
+          " 
+          <input type=\"text\" id = \"joe\" name=\"daterange\" value=\"", dr[1], " - ", dr[2], "\" />
+          
+          <script type=\"text/javascript\">
+          $(function() {
+          $('input[name=\"daterange\"]').daterangepicker();
+// the below two lines were written by joe, trying to get the input list
+// to get updated, but it's not working
+          document.getElementById(\"joe\").onchange = function() {
+        var time = document.getElementById(\"joe\").value;
+        Shiny.onInputChange(\"daterange\", time);
+    };
+          });
+          </script>"
+        )
+        )
+    )
+  })
 }
 
 # Run the application 
