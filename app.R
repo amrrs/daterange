f45d9e333922d2068499c3ca40c65412285a9184
joebrew <joebrew@gmail.com>
2018-02-10 23:38:59 +0100

    go the input

--- app.R
+++ app.R
@@ -11,31 +11,59 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        helpText('The below needs to be replaced with a date range input that highlights all the days between the first and second day'),
-         dateRangeInput("date_range",
-                     "Date range input")
-      ),
+  
+  tags$div(
+    
+    HTML(
+      '<!-- Include Required Prerequisites -->
+      <script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
+      <script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap/3/css/bootstrap.css" />
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         textOutput("date_range_values")
+      <!-- Include Date Range Picker -->
+      <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+      <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />'
+    )
+    ),
+  
+  # Application title
+  titlePanel("Date range"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      tags$div(
+        HTML(
+          " 
+          <input type=\"text\" name=\"daterange\" value=\"01/05/2015 - 01/31/2015\" />
+          
+          <script type=\"text/javascript\">
+          $(function() {
+          $('input[name=\"daterange\"]').daterangepicker();
+// the below two lines were written by joe, trying to get the input list
+// to get updated, but it's not working
+          var vv = ('input[name=\"daterange\"]').daterangepicker();
+          Shiny.onInputChange('daterange', vv);
+          });
+          </script>")
+        )
+      ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      textOutput("date_range_values")
+    )
       )
-   )
-)
+      )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$date_range_values <- renderText({
-      as.character(input$date_range)
-   })
+  
+  output$date_range_values <- renderText({
+    # This is working on the start of the app, but doesn't update
+    as.character(input$daterange)
+  })
+
 }
 
 # Run the application 
