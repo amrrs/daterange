1fcd5d826a9f68045472aa0d57422d629c3c82c3
amrrs <amrrs@outlook.com>
2018-02-16 16:08:13 +0530

    Update app.R

--- app.R
+++ app.R
@@ -94,7 +94,7 @@ server <- function(input, output,session) {
                     var time = document.getElementById(\"daterange12container\").value;
     Shiny.onInputChange(\"daterange12\", time);
         val = document.getElementById(\"daterange12container\").value;
-          Shiny.onInputChange("daterange12",val);
+          Shiny.onInputChange('daterange12',val);
 	//Shiny.onInputChange('joe',obj.date1);
 	alert('date1: ' + obj.date1 + ' / date2: ' + obj.date2);
 });
