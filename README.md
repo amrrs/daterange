6b50026f2c94287014145060220b31c1c2c6c8b3
joebrew <joebrew@gmail.com>
2018-02-16 01:20:27 +0100

    reproducible issue

--- README.md
+++
@@ -1,2 +0,0 @@
-# daterange
-Minimal date range picker
--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+# Using https://longbill.github.io/jquery-date-range-picker/
 library(shiny)
 library(shinydashboard)
 
@@ -5,78 +6,107 @@ header <- dashboardHeader(title="Date picker")
 sidebar <- dashboardSidebar()
 body <- dashboardBody(
   
-  # tags$head(tags$script(src = 'jquery.min.js')),
-  tags$head(tags$script(src = 'moment.min.js')),
-  # tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css")),
-  tags$head(tags$script(src = 'daterangepicker.js')),
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "daterangepicker.css")),
+  tags$head(tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css')),
+  tags$head(tags$link(rel = 'stylesheet', type = 'text/css', href = 'dist/daterangepicker.min.css')),
+  tags$head(tags$script(src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js', type = 'text/javascript')),
+  tags$head(tags$script(src = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.16.0/moment.min.js', type = 'text/javascript')),
+  tags$head(tags$script(src = 'src/jquery.daterangepicker.js')),
+  tags$head(tags$script(src = 'demo.js')),
+  tags$head(tags$script(src = 'src/jquery.daterangepicker.js')),
+  
+  
+  tags$head(tags$style(HTML("
+                            
+                            #wrapper
+                            {
+                            width:600px;
+                            margin:0 auto;
+                            color:#333;
+                            font-family:Tahoma,Verdana,sans-serif;
+                            line-height:1.5;
+                            font-size:14px;
+                            }
+                            .demo { margin:30px 0;}
+                            .date-picker-wrapper .month-wrapper table .day.lalala { background-color:orange; }
+                            .options { display:none; border-left:6px solid #8ae; padding:10px; font-size:12px; line-height:1.4; background-color:#eee; border-radius:4px;}
+                            
+                            "))),
   
   # Application title
-  titlePanel("Date range"),
   
   fluidPage(
-    fluidRow(column(4,uiOutput('date_ui')),
-             column(8,
-                    textOutput("date_range_values")))
+    
+    fluidRow(column(8,
+                    uiOutput('date_ui')),
+             column(4,
+                    h3('Table of inputs'),
+                    tableOutput('show_inputs')))
   ))
 
 
 ui <- dashboardPage(header, sidebar, body)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
   
-  date_range <- reactiveVal(c(Sys.Date() - 7,
-                              Sys.Date() + 7))
-  
-  observeEvent(input$daterange, {
-    message('input date range is ', input$daterange)
-    idr <- input$daterange
-    idr <- unlist(strsplit(idr, ' - '))
-    idr <- as.Date(idr, format = '%m/%d/%Y')
-    date_range(idr)
+  date_range <- reactiveVal('Pick some dates')
+  observeEvent(input$daterange12,{
+    date_input <- input$daterange12
+    message('Dates changed. They are: ')
+    print(input$daterange12)
+    message('Joe:')
+    print(input$joe)
+    date_range(date_input)
   })
   
-  output$date_range_values <- renderText({
-    as.character(date_range())
+  AllInputs <- reactive({
+    x <- reactiveValuesToList(input)
+    data.frame(
+      names = names(x),
+      values = unlist(x, use.names = FALSE)
+    )
+  })
+  
+  output$show_inputs <- renderTable({
+    AllInputs()
   })
   
   output$date_ui <- renderUI({
-    dr <- date_range()
-    dr <- as.Date(dr)
-    print(dr)
-    dr <- format(dr, '%m/%d/%Y')
-    tags$div(
-      HTML(
-        paste0(
-          " 
-          
-          <div class=\"form-group shiny-input-container\" style=\"width: 60%; \">
-<label for=\"date_range\">Pick a date range for analysis</label>
-          <input type=\"text\" class=\"form-control\" id = \"joe\" name=\"daterange\" value=\"", dr[1], " - ", dr[2], "\" />
-       </div>   
-          <script type=\"text/javascript\">
-          $(function() {
-          $('input[name=\"daterange\"]').daterangepicker({
-
-          \"linkedCalendars\": false,
-          \"alwaysShowCalendars\": true,
-          \"autoApply\": true,
-          \"opens\": \"center\",
-
+    
+    dr <- paste0(Sys.Date(), ' to ', Sys.Date() + 14)
+    fluidPage(
+      tags$div(HTML("
+                    
+                    <div id='daterange12container' style=\"width:456px;\">
+                    <input id=\"daterange12\" name=\"joe\" type=\"text\" class=\"form-control\" value=\"",dr, "\"/>
+                    
+                    </div>
+                    <script type=\"text/javascript\">
+                    $(function() {
+                    $('#daterange12').dateRangePicker(
+                    {
+                    inline:true,
+                    container: '#daterange12container', 
+                    alwaysOpen:true
+                    });
+                    
+                    // JOE:
+                    
+                    $('#daterange12').on('change', function() {
+                    var time = document.getElementById(\"daterange12container\").value;
+    Shiny.onInputChange(\"daterange12\", time);
+        val = document.getElementById(\"daterange12container\").value;
+                    Shiny.onInputChange(\"daterange12\",val);
+});
           });
-// JOE:
-          document.getElementById(\"joe\").onchange = function() {
-        var time = document.getElementById(\"joe\").value;
-        Shiny.onInputChange(\"daterange\", time);
-    };
-          });
-          </script>"
-        )
-      )
-    )
-  })
-}
+          </script>
+
+                                  "))
+                    )
+                    })
+  
+  
+                    }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
